78b0fa26406134f8ab2d6428b3f3a5e840af712d
Dipen Shah <Dips365@hotmail.com>
2020-02-28 00:18:41 +0530

    Commit

---
+++ SingleAppPartPage/src/webparts/singleAppPartPage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISingleAppPartPageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SingleAppPartPageWebPartStrings' {
+  const strings: ISingleAppPartPageWebPartStrings;
+  export = strings;
+}
