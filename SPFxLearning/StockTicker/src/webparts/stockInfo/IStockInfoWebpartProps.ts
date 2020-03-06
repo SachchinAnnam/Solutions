5cc2a3863a6028975fc81b7a7fb56292fa86ab74
Dipen Shah <dipens@stridely.local>
2020-03-06 15:26:38 +0530

    Removed StockInfo WebPart from Learning

--- SPFxLearning/StockTicker/src/webparts/stockInfo/IStockInfoWebpartProps.ts
+++
@@ -1,8 +0,0 @@
-import { IPropertyFieldDimension } from "sp-client-custom-fields/lib/PropertyFieldDimensionPicker";
-
-export interface IStockInfoWebpartProps{
-  stock: string;
-  lang: string;
-  region: string;
-  dimension: IPropertyFieldDimension;
-}
